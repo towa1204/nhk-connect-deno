838eb98165beb90cd44759d856e4a1de10eb105d
towa1204 <lsadsfj@gmail.com>
2024-06-04 22:17:15 +0900

    update

--- date.ts
+++ date.ts
@@ -16,18 +16,38 @@ export function getWeekDates(date: Date) {
 }
 
 /**
- * DateオブジェクトをYYYY-MM-DDに変換する
+ * DateオブジェクトをJSTのYYYY-MM-DDに変換する
  * @param date Dateオブジェクト
  * @returns YYYY-MM-DD
  */
-export function convertNHKDate(date: Date) {
-  const formattedDate = date
-    .toLocaleDateString("ja-JP", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    })
-    .split("/")
-    .join("-");
-  return formattedDate;
+export function convertNHKDateFormat(dateObject: Date) {
+  const { year, month, date } = getJSTDateParts(dateObject);
+  return `${year}-${month}-${date}`;
+}
+
+/**
+ * DateオブジェクトをJSTに変換したうえで各日時ごとに取得する
+ * @param rawDate JSTに変換したいDateオブジェクト
+ * @returns JSTの{年,月,日,時,分,秒}
+ */
+export function getJSTDateParts(dateObject: Date) {
+  // 日本時間（JST）に変換
+  // UTC+9時間を加算
+
+  // ローカルタイムをUTCタイムスタンプに変換
+  const utcTimestamp = dateObject.getTime();
+  // 9時間をミリ秒に変換
+  const jstOffset = 9 * 60 * 60 * 1000;
+
+  const jstTimestamp = utcTimestamp + jstOffset;
+  const jstDate = new Date(jstTimestamp);
+
+  return {
+    "year": jstDate.getUTCFullYear(),
+    "month": ("0" + (jstDate.getUTCMonth() + 1)).slice(-2), // 月は0から始まるため1を加算
+    "date": ("0" + jstDate.getUTCDate()).slice(-2),
+    "hours": ("0" + jstDate.getUTCHours()).slice(-2),
+    "minutes": ("0" + jstDate.getUTCMinutes()).slice(-2),
+    "seconds": ("0" + jstDate.getUTCSeconds()).slice(-2),
+  };
 }
--- main.ts
+++ main.ts
@@ -1,18 +1,50 @@
 import "https://deno.land/std@0.223.0/dotenv/load.ts";
-import { ProgramListReq } from "./types.ts";
+import secrets from "./secrets.json" with { type: "json" };
+import { NotifyRequest, ProgramListReq } from "./types.ts";
 import { fetchProgramList } from "./api/nhk_client.ts";
+import { convertNHKDateFormat } from "./date.ts";
 
-const apiKey = Deno.env.get("API_KEY");
-if (apiKey == null) {
-  throw new Error(".envにAPI_KEYが見つかりません。");
+// Deno.cronから呼び出す想定
+await notifyDaily();
+
+async function notifyDaily() {
+  /**
+   * 0. secrets.jsonから設定情報を取得
+   */
+  const broadcastArea = secrets.area;
+  const broadcastServices = secrets.services;
+  const NHKAPIKey = secrets.nhkAPIKey;
+  const programKeywords = secrets.subscribePrograms.map((program) =>
+    program.keyword
+  );
+
+  /**
+   * 1. 番組情報の取得
+   * Serviceの数だけリクエストを実施
+   */
+  const nowNHKDateFormat = convertNHKDateFormat(new Date());
+  for (const broadcastService of broadcastServices) {
+    const serviceProgramList = await fetchProgramList({
+      area: broadcastArea,
+      service: broadcastService,
+      date: nowNHKDateFormat,
+      apikey: NHKAPIKey,
+    });
+    console.log(serviceProgramList);
+  }
+
+  /**
+   * 2. 視聴番組情報の取得
+   * 番組情報から「視聴番組」かつ「実行日時から24時間以内に放映される番組」を抜き出す
+   */
+
+  /**
+   * 3. 通知
+   * APIを使用して放送情報を通知する
+   * 通知アプリによらないインタフェースにする
+   */
+  //
 }
 
-const req: ProgramListReq = {
-  area: "130",
-  service: "tv",
-  date: "2024-05-06",
-  apikey: apiKey,
-};
-
-const res = await fetchProgramList(req);
-console.log(res);
+// function notifyWeekly() {
+// }
