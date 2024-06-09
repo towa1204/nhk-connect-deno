b426855d77734447937c401ce4fff3930d70e8bc
towa1204 <lsadsfj@gmail.com>
2024-06-09 15:24:02 +0900

    cron式が誤っていた

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ Deno.cron("notification-daily job", "30 22 * * *", async () => {
   console.log("### 日次通知処理 END ###");
 });
 
-Deno.cron("notification-weekly job", "30 22 * * 0", async () => {
+Deno.cron("notification-weekly job", "30 22 * * 1", async () => {
   console.log("### 週次通知処理 START ###");
   await notifyWatchPrograms("weekly");
   console.log("### 週次通知処理 END ###");
