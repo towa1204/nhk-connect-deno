f4996baac883183b8b723a2a1c965ec639e3f8e7
towa1204 <lsadsfj@gmail.com>
2024-07-06 14:38:14 +0900

    frontend-appの導入

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
 import { notifyWatchPrograms } from "./service.ts";
+
+const app = new Hono();
+
+app.use("/*", serveStatic({ root: "./frontend-app/dist/" }));
+
+Deno.serve(app.fetch);
 
 Deno.cron("notification-daily job", "30 22 * * *", async () => {
   console.log("### 日次通知処理 START ###");
