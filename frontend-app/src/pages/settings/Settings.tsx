c48b39704917354084cac778039634416cabae49
towa1204 <lsadsfj@gmail.com>
2024-07-14 20:20:51 +0900

    Selectをコンポーネント化したので元のコードを削除

--- frontend-app/src/pages/settings/Settings.tsx
+++ frontend-app/src/pages/settings/Settings.tsx
@@ -41,26 +41,6 @@ export default function Settings() {
           options={options}
           setValue={setSelectedArea}
         />
-        {/* <div className="relative w-full  max-w-sm">
-          <select
-            className="block w-full appearance-none rounded-lg border border-gray-200 bg-gray-50 px-4 py-2 text-base text-gray-500 hover:border-gray-500"
-            value={selectedArea}
-            onChange={(e) => setSelectedArea(e.target.value)}
-          >
-            <option value="US">United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 hover:border-gray-500">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-              <path
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
-        </div> */}
 
         <div className="mt-3">
           <div className="relative w-full max-w-sm">
